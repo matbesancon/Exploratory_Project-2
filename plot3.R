ae4563a0009aeff90d79975b6317888918460fcc
MathieuB <mathieu.besancon@gmail.com>
2014-09-07 22:58:32 +0200

    Creating the plot 3 with ggplot2 and extraGrid

--- plot3.R
+++ plot3.R
@@ -4,6 +4,8 @@ SCC <- readRDS("Source_Classification_Code.rds")
 
 # Loading the ggplot2 library
 library(ggplot2)
+library(extraGrid)
+
 # Extracting the data for Baltimore
 balti_data<-subset(part_data,fips=="24510")
 
@@ -19,24 +21,15 @@ agg_non_point<-aggregate(sub_non_point$Emissions,list(sub_non_point$year),FUN=su
 agg_onroad<-aggregate(sub_onroad$Emissions,list(sub_onroad$year),FUN=sum)
 agg_non_road<-aggregate(sub_non_road$Emissions,list(sub_non_road$year),FUN=sum)
 
+plot_point<-qplot(Group.1,x,main="Particle emissions for Point",data=agg_point,xlab="Year",ylab="Particle emissions (T)",geom=c("point","line")) +geom_line()
+plot_non_point<-qplot(Group.1,x,main="Particle emissions for Non-Point",data=agg_non_point,xlab="Year",ylab="Particle emissions (T)",geom=c("point","line"))+geom_line()
+plot_onroad<-qplot(Group.1,x,main="Particle emissions for On-Road",data=agg_onroad,xlab="Year",ylab="Particle emissions (T)",geom=c("point","line"))+geom_line()
+plot_non_road<-qplot(Group.1,x,main="Particle emissions for Point",data=agg_non_road,xlab="Year",ylab="Particle emissions (T)",geom=c("point","line"))+geom_line()
+
 #Creating a PNG file
 png(filename='plot3.png')
-
 # Plotting the 4 different types per year
-par(mfrow=c(2,2))
-plot(agg_point$Group.1,agg_point$x,main="Particle emissions for Point",xlab="Year",ylab="Particle emission (T)",pch="+")
-lines(agg_point$Group.1,agg_point$x,col="purple")
-
-mtext("Comparison of particle emissions the for types across years ")
-
-plot(agg_non_point$Group.1,agg_non_point$x,main="Particle emissions for Non-Point",xlab="Year",ylab="Particle emission (T)",pch="+")
-lines(agg_non_point$Group.1,agg_non_point$x,col="red")
-
-plot(agg_onroad$Group.1,agg_onroad$x,main="Particle emissions for On Road",xlab="Year",ylab="Particle emission (T)",pch="+")
-lines(agg_onroad$Group.1,agg_onroad$x,col="blue")
-
-plot(agg_non_road$Group.1,agg_non_road$x,main="Particle emissions for Non-Road",xlab="Year",ylab="Particle emission (T)",pch="+")
-lines(agg_non_road$Group.1,agg_non_road$x,col="orange")
+grid.arrange(plot_point,plot_non_point,plot_onroad,plot_non_road,cols=2)
 
 dev.off()
 
