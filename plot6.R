c0c2e3f37f2bdd4855f96f3d3313cf9a3ec9a7e3
MathieuB <mathieu.besancon@gmail.com>
2014-09-09 20:01:28 +0200

    Finishing plot 6
    Using ggplot(), facets

--- plot6.R
+++ plot6.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 # Loading the data
 part_data=readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
@@ -9,5 +11,21 @@ two_cities_data<-subset(part_data,(fips=="24510")|(fips=="06037"))
 index_car<-which(grepl('Motor',SCC$Short.Name)==TRUE)
 sub_car<-SCC[index_car,]
 
-#Subsetting the Baltimore data emissions from motor vehicles
-car_data<-two_cities_data[which(balti_data$SCC%in%sub_car$SCC),]
+#Subsetting the cities' data emissions from motor vehicles
+car_data<-two_cities_data[which(two_cities_data$SCC%in%sub_car$SCC),]
+agg_car<-aggregate(Emissions~year+fips,data=car_data,FUN=sum)
+agg_car$City<-NA
+for (i in 1:length(agg_car[,1])){
+        if (agg_car[i,2]=="24510"){
+                agg_car[i,4]<-"Baltimore"
+        }else{agg_car[i,4]<-"Los Angeles"}
+} 
+
+p<-ggplot(data=agg_car,aes(year,Emissions,colour=factor(City)))
+p<-p+geom_point(size=4,shape=7)
+p<-p+geom_line(size=0.8)
+p<-p+facet_grid(fips~.,scales="free_y")
+
+png(filename='plot6.png')
+p
+dev.off()
