459159c026e293451f15178b2b82c1b2eceb8247
MathieuB <mathieu.besancon@gmail.com>
2014-09-09 18:35:28 +0200

    Subsetting the data related to car emissions in plot 6

--- plot6.R
+++ plot6.R
@@ -4,3 +4,10 @@ SCC <- readRDS("Source_Classification_Code.rds")
 
 #Subsetting data from Baltimore and Los Angeles County
 two_cities_data<-subset(part_data,(fips=="24510")|(fips=="06037"))
+
+# Subsetting the SCC data including the words "Motor vehicle" in the short name into 'sub_car'
+index_car<-which(grepl('Motor',SCC$Short.Name)==TRUE)
+sub_car<-SCC[index_car,]
+
+#Subsetting the Baltimore data emissions from motor vehicles
+car_data<-two_cities_data[which(balti_data$SCC%in%sub_car$SCC),]
